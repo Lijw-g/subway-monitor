685c36c5f26c21d7b1de26d062168743fc1127dd
lijiwen <lijiwen@guazi.com>
2019-09-09 19:18:39 +0800

    ewrf

---
+++ src/main/java/com/subway/monitor/SubwayMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.subway.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubwayMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubwayMonitorApplication.class, args);
+    }
+
+}
