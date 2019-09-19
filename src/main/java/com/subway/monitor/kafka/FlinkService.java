ae731ad71b67a95afdd045a0a484d21780078c8f
lijiwen <lijiwen@guazi.com>
2019-09-19 22:58:56 +0800

    添加mongo

--- src/main/java/com/subway/monitor/kafka/FlinkService.java
+++ src/main/java/com/subway/monitor/kafka/FlinkService.java
@@ -1,13 +1,8 @@
 package com.subway.monitor.kafka;
 
-import com.subway.monitor.model.Book;
-import com.subway.monitor.service.MongoDbService;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.bson.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
@@ -22,30 +17,24 @@ import java.util.Optional;
  * @date 2018/8/3
  */
 @Component
-public class KafkaConsumer {
+public class FlinkService {
 
     private Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
-    @Autowired
-    private MongoDbService mongoDbService;
-    @Autowired
-    private MongoTemplate mongoTemplate;
+
     /**
      * 监听kafka.tut 的topic，不做其他业务
      *
      * @param record
      * @param topic  topic
      */
-    @KafkaListener(id = "data",groupId="dataBase", topics = "test")
+    @KafkaListener(groupId="ff", topics = "test")
     public void listen(ConsumerRecord<?, ?> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
         Optional<?> kafkaMessage = Optional.ofNullable(record.value());
         if (kafkaMessage.isPresent()) {
             Object message = kafkaMessage.get();
-            Document document = new Document();
-            document.append("topic", topic).append("Record", record).append("mesage", message.toString());
-            mongoTemplate.insert(document, "subwayData");
-            logger.info("Receive： +++++++monitor++++++++ Topic:" + topic);
-            logger.info("Receive： ++++++++monitor+++++++ Record:" + record);
-            logger.info("Receive： ++++++++monitor+++++++ Message:" + message);
+            logger.info("Receive： +++++++flink++++++++ Topic:" + topic);
+            logger.info("Receive： ++++++++flink+++++++ Record:" + record);
+            logger.info("Receive： ++++++++flink+++++++ Message:" + message);
         }
     }
 
